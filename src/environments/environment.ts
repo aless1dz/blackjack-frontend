0b5b35cec84cf2aa119e618ec51e40b89f0beb63
alejandro <jorgerenteriareyes4@gmail.com>
2025-08-11 16:48:37 -0600

    ultimo cambio 04:48

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl : 'https://650a1d5c79a8.ngrok-free.app/api',
-  apiUrlSockets: 'https://650a1d5c79a8.ngrok-free.app'
+  apiUrl : 'https://c51653070bd0.ngrok-free.app/api',
+  apiUrlSockets: 'https://c51653070bd0.ngrok-free.app'
 };
